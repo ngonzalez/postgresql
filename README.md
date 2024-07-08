325cdd6ddb7be7dcd109cb6afe11b615f0198600
Nicolas GONZALEZ <nicolas@thedeepcore.com>
2024-07-08 13:29:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#### postgresql-docker
+
+```shell
+docker build . -f Dockerfile -t postgresql
+```
+
+```shell
+docker run --rm -it -p 5432:5432 postgresql
+``` 
+
+```shell
+export POSTGRESQL_SERVER_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresql`
+```
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+function start_postgresql {
+	su -c '/usr/lib/postgresql/$POSTGRESQL_VERSION/bin/postgres "-p" $POSTGRESQL_PORT "-D" "/var/lib/postgresql/$POSTGRESQL_VERSION/data" "-c" "config_file=/etc/postgresql/$POSTGRESQL_VERSION/main/postgresql.conf"' $USER
+}
+
+function usage {
+	echo "usage: entrypoint.sh [--server|--help]"
+}
+
+if [ "$1" != "" ]; then
+	case $1 in
+		-s | --server )   start_postgresql
+			                exit
+			                ;;
+		-h | --help )     usage
+                			exit
+                			;;
+	esac
+	shift
+fi
+
+exec "$@"
